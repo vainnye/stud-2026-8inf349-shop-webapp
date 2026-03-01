0388ed13e30c3795650c297b456ea5a1cced3c93
vainnye <vianney.jcqmt@gmail.com>
2026-02-28 21:52:52 -0500

    orm model done
    (pas certain des contraintes not null, j'ai laissé les valeurs par
    défaut)

---
+++ src/model.py
@@ -0,0 +1,66 @@
+from peewee import (
+    AutoField,
+    BooleanField,
+    CharField,
+    DecimalField,
+    ForeignKeyField,
+    IntegerField,
+    Model,
+    SqliteDatabase,
+    TextField,
+)
+
+db = SqliteDatabase("database.db")
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Product(BaseModel):
+    id = AutoField(primary_key=True)
+    name = CharField()
+    description = TextField()
+    image = CharField()
+    in_stock = BooleanField()
+    weight = IntegerField()
+    price = DecimalField(decimal_places=2)
+
+
+class Order(BaseModel):
+    id = AutoField(primary_key=True)
+    total_price = DecimalField(decimal_places=2)
+    total_price_tax = DecimalField(decimal_places=2)
+    email = CharField()
+
+
+class OrderProduct(BaseModel):
+    order_id = ForeignKeyField(Order, primary_key=True, backref="products")
+    product_id = ForeignKeyField(Product, primary_key=True, backref="orders")
+    quantity = IntegerField()
+
+
+class CreditCard(BaseModel):
+    order_id = ForeignKeyField(Order, primary_key=True, backref="credit_card")
+    name = CharField()
+    number = CharField()
+    expiration_year = IntegerField()
+    cvv = CharField()
+    expiration_month = IntegerField()
+
+
+class ShippingInformation(BaseModel):
+    order_id = ForeignKeyField(Order, primary_key=True, backref="shipping_information")
+    country = CharField()
+    address = CharField()
+    postal_code = CharField()
+    city = CharField()
+    province = CharField()
+
+
+class Transaction(BaseModel):
+    order_id = ForeignKeyField(Order, primary_key=True, backref="transaction")
+    amount = DecimalField(decimal_places=2)
+    credit_card_id = ForeignKeyField(CreditCard)
+    shipping_information_id = ForeignKeyField(ShippingInformation)
