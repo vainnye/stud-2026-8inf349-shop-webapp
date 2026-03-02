a13bc1dce04a7d4771d6988ae639fddede8c0b20
vainnye <vianney.jcqmt@gmail.com>
2026-03-02 08:55:58 -0500

    added __str__ methods to products

--- src/shop_webapp/model.py
+++ src/shop_webapp/model.py
@@ -28,12 +28,20 @@ class Product(BaseModel):
     weight = IntegerField()
     price = DecimalField(decimal_places=2)
 
+    def __str__(self):
+        return f"Product(id={self.id}, name='{self.name}', price={self.price})"
+
 
 class Order(BaseModel):
     id = AutoField(primary_key=True)
     total_price = DecimalField(decimal_places=2)
     total_price_tax = DecimalField(decimal_places=2)
     email = CharField()
+
+    def __str__(self):
+        return (
+            f"Order(id={self.id}, email='{self.email}', total_price={self.total_price})"
+        )
 
 
 class OrderProduct(BaseModel):
@@ -44,6 +52,9 @@ class OrderProduct(BaseModel):
     class Meta:
         primary_key = CompositeKey("order", "product")
 
+    def __str__(self):
+        return f"OrderProduct(order_id={self.order.id}, product_id={self.product.id}, quantity={self.quantity})"
+
 
 class CreditCard(BaseModel):
     order = ForeignKeyField(Order, primary_key=True, backref="credit_card")
@@ -52,6 +63,9 @@ class CreditCard(BaseModel):
     expiration_year = IntegerField()
     cvv = CharField()
     expiration_month = IntegerField()
+
+    def __str__(self):
+        return f"CreditCard(order_id={self.order.id}, name='{self.name}', number='****{self.number[-4:]}')"
 
 
 class ShippingInformation(BaseModel):
@@ -62,9 +76,15 @@ class ShippingInformation(BaseModel):
     city = CharField()
     province = CharField()
 
+    def __str__(self):
+        return f"ShippingInformation(order_id={self.order.id}, address='{self.address}', city='{self.city}', country='{self.country}')"
+
 
 class Transaction(BaseModel):
     order = ForeignKeyField(Order, primary_key=True, backref="transaction")
     amount = DecimalField(decimal_places=2)
     credit_card = ForeignKeyField(CreditCard)
     shipping_information = ForeignKeyField(ShippingInformation)
+
+    def __str__(self):
+        return f"Transaction(order_id={self.order.id}, amount={self.amount})"
